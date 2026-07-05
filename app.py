1693edb2ab3c0ebded81dc7109455b2915737a28
Raksha <biradarrutuja125@gmail.com>
2026-07-06 00:53:16 +0530

    Completed user authentication module

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, redirect, url_for, render_template, request, flash
+import os
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+from flask import Flask, redirect, url_for, render_template, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
-from flask_dance.contrib.google import make_google_blueprint
-from werkzeug.security import generate_password_hash
+from flask_dance.contrib.google import make_google_blueprint, google
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # -------------------------------
 # Initialize Extensions
 # -------------------------------
 db = SQLAlchemy()
 login_manager = LoginManager()
-
 
 # -------------------------------
 # User Model
@@ -63,6 +66,9 @@ def create_app():
 
     login_manager.login_view = "login"
 
+    # app = Flask(__name__)
+    app.secret_key = "CyberShield@2026"
+
     # ---------------------------------------
     # Google OAuth
     # ---------------------------------------
@@ -70,8 +76,9 @@ def create_app():
         client_id="21441301027-u9hd397kltm2o23dhknd82895is2mpq2.apps.googleusercontent.com",
         client_secret="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",
         scope=[
-            "profile",
-            "email"
+            "openid",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/userinfo.profile",
         ]
     )
 
@@ -94,7 +101,30 @@ def create_app():
     # Login
     # ---------------------------------------
     @app.route("/login")
+    @app.route("/login", methods=["GET", "POST"])
     def login():
+
+        if request.method == "POST":
+
+            email = request.form["email"]
+            password = request.form["password"]
+
+            user = User.query.filter_by(email=email).first()
+
+            if user and check_password_hash(user.password, password):
+
+                session["user_id"] = user.id
+                session["username"] = user.username
+                session["email"] = user.email
+                session["login_type"] = "normal"
+
+                flash("Login Successful")
+
+                return redirect(url_for("dashboard"))
+
+            else:
+                flash("Invalid Email or Password")
+
         return render_template("login.html")
 
     # ---------------------------------------
@@ -152,49 +182,81 @@ def create_app():
 
             return redirect(url_for("login"))
 
-        return render_template("register.html")
+        return render_template("login.html")
 
     # ---------------------------------------
     # Logout
     # ---------------------------------------
     @app.route("/logout")
     def logout():
-        return redirect(url_for("login"))
+       session.clear()
 
+       flash("Logged Out Successfully")
+
+       return redirect(url_for("login"))
     # ---------------------------------------
     # Dashboard
     # ---------------------------------------
     @app.route("/dashboard")
     def dashboard():
 
+    # --------------------------
+    # Normal Login
+    # --------------------------
+        if "user_id" in session:
+
+            return render_template(
+                "dashboard.html",
+                username=session["username"]
+            )
+
+    # --------------------------
+    # Google Login
+    # --------------------------
+        if google.authorized:
+
+            resp = google.get("/oauth2/v2/userinfo")
+
+            if not resp.ok:
+                return "Failed to fetch user information."
+
+            info = resp.json()
+
+            user = User.query.filter_by(email=info["email"]).first()
+
+            if not user:
+
+                user = User(
+                    google_id=info["id"],
+                    name=info["name"],
+                    email=info["email"],
+                    picture=info["picture"]
+                )
+
+                db.session.add(user)
+                db.session.commit()
+
+            session["user_id"] = user.id
+            session["username"] = user.name
+            session["email"] = user.email
+            session["login_type"] = "google"
+
+            return render_template(
+                "dashboard.html",
+                username=user.name
+            )
+
+        return redirect(url_for("login"))
+    # ---------------------------------------
+    # Google Login Route
+    # ---------------------------------------
+    @app.route("/google-login")
+    def google_login():
+
         if not google.authorized:
             return redirect(url_for("google.login"))
 
-        resp = google.get("/oauth2/v2/userinfo")
-
-        if not resp.ok:
-            return "Failed to fetch user information."
-
-        info = resp.json()
-
-        user = User.query.filter_by(email=info["email"]).first()
-
-        if not user:
-
-            user = User(
-                google_id=info["id"],
-                name=info["name"],
-                email=info["email"],
-                picture=info["picture"]
-            )
-
-            db.session.add(user)
-            db.session.commit()
-
-        return render_template(
-            "dashboard.html",
-            user=user
-        )
+        return redirect(url_for("dashboard"))
 
     # ---------------------------------------
     # Upload Dataset
